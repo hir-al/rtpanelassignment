e73cee5a30264273d660557a8921a8f4914f28fd
hir-al <hiral.makwana@rocketmail.com>
2014-06-04 21:53:01 +0530

    applied stuctured format and changed text domain

--- custom-posts/portfolio.php
+++ custom-posts/portfolio.php
@@ -1,30 +1,43 @@
-<?php 
-function portfolio_theme_custom_posts(){
+<?php
+
+function portfolio_theme_custom_posts()
+{
 	$labels = array(
-	  'name' => _x('Portfolios', 'Portfolios','templatemela'),
-	  'singular_name' => _x('Portfolio', 'portfolio','templatemela'),
-	  'add_new' => _x('Add Portfolio', 'portfolio','templatemela'),
-	  'add_new_item' => __('Add New Portfolio','templatemela'),
-	  'edit_item' => __('Edit Portfolio','templatemela'),
-	  'new_item' => __('New Portfolio','templatemela'),
-	  'view_item' => __('View Portfolio','templatemela'),
-	  'search_items' => __('Search Portfolio','templatemela'),
-	  'not_found' =>  __('No Portfolio found','templatemela'),
-	  'not_found_in_trash' => __('No Portfolio found in Trash','templatemela'), 
-	  'parent_item_colon' => ''
+		'name' => _x('Portfolios', 'Portfolios', 'rtPanel') ,
+		'singular_name' => _x('Portfolio', 'portfolio', 'rtPanel') ,
+		'add_new' => _x('Add Portfolio', 'portfolio', 'rtPanel') ,
+		'add_new_item' => __('Add New Portfolio', 'rtPanel') ,
+		'edit_item' => __('Edit Portfolio', 'rtPanel') ,
+		'new_item' => __('New Portfolio', 'rtPanel') ,
+		'view_item' => __('View Portfolio', 'rtPanel') ,
+		'search_items' => __('Search Portfolio', 'rtPanel') ,
+		'not_found' => __('No Portfolio found', 'rtPanel') ,
+		'not_found_in_trash' => __('No Portfolio found in Trash', 'rtPanel') ,
+		'parent_item_colon' => ''
 	);
 	$args = array(
-	  'labels' => $labels,
-	  'public' => true,
-	  'publicly_queryable' => true,
-	  'show_ui' => true, 
-	  'query_var' => true, 
-	  'capability_type' => 'post', 
-	  'menu_position' => null,
-	  'menu_icon' => 'dashicons-portfolio',
-	  'rewrite' => array('slug'=>'portfolio','with_front'=>''),
-	  'supports' => array('title','editor','author','thumbnail','comments')
-	); 
-	register_post_type('portfolio',$args);	
+		'labels' => $labels,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'query_var' => true,
+		'capability_type' => 'post',
+		'menu_position' => null,
+		'menu_icon' => 'dashicons-portfolio',
+		'rewrite' => array(
+			'slug' => 'portfolio',
+			'with_front' => ''
+		) ,
+		'supports' => array(
+			'title',
+			'editor',
+			'author',
+			'thumbnail',
+			'comments'
+		)
+	);
+	register_post_type('portfolio', $args);
 }
-add_filter('init', 'portfolio_theme_custom_posts' );
+
+add_filter('init', 'portfolio_theme_custom_posts');
+?>
